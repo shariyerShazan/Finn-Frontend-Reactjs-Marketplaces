3eb83d49050cfcc3a2a422c88a1118900f5d2504
AnisulHaqueNiloy <niloy2931@gmail.com>
2026-03-13 15:55:47 +0600

    url changed

--- src/redux/api/customBaseQuery.ts
+++ src/redux/api/customBaseQuery.ts
@@ -1,11 +1,11 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const rawBaseQuery = fetchBaseQuery({
-  baseUrl: "https://api.www.zen-buy.com",
+  baseUrl: "https://api.zen-buy.com",
   // baseUrl:"http://localhost:3002",
   credentials: "include",
 });
-
+// url changed
 export const customBaseQuery = async (args: any, api: any, extraOptions: any) => {
   // get language from localStorage
   const lang = localStorage.getItem("lang");
